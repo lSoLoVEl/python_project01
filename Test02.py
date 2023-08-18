257639ed529adc0fcb1a1e8c3612ba86057d17e4
lSoLoVEl <s6519410044@sau.ac.th>
2023-08-18 11:15:40 +0700

    code V.1 : ลองสร้างไป3ไฟล์

---
+++ Test02.py
@@ -0,0 +1,8 @@
+#การแสดงผลข้อมูลใดๆ ทางหน้าจอด้วย python ใช้ฟังก์ชัน print()
+print('yoyo', end=' ')
+print("Dota2 suck game")    #string
+print(55555)                #number
+print(True)                 #boolean
+print('false')              #string
+print(6+9)                  #Expression นิพจน์ 
+
---
+++ Test03.py
@@ -0,0 +1,8 @@
+print('aaa') ; print('bbb')
+print('ccc')
+print('ddd\neeee\nfff')
+print('ddd\teeee\tfff')
+print('welcome"Sau"')
+print('''
+Helooooosdoas
+      hahaaha''')
